727d16cb9a655096f768d118f5b853ee757f2d82
frick.james <jamesfrick100@yahoo.com>
2021-01-13 21:01:56 -0500

    INI - Created branch for express and templating.

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const ejs = require('ejs');
 
-//create a server object;
-http.createServer(function (req, res) { //req = request, res = response
+const app = express(); // create express app
+app.set('view engine', 'ejs');  // use ejs for view engine -- seems pug is popular too
 
-    let path = url.parse(req.url).pathname;
-        
-        switch (path) {
-            case '/':
-                fs.readFile('homepage.html', function(err, data) {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(data);
-                    return res.end(); //end the response
-                });
-                break;
-            case '/about':
-                fs.readFile('about.html', function(err, data) {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(data);
-                    return res.end(); //end the response
-                });
-                break;
-            default:
-                res.writeHead(404);
-                res.write('Route not found');
-                res.end();
-        }
-}).listen(8080); //the server object listens on port 8080
+
+app.get('/', (req, res) => {
+    let x = 15;
+    res.render('homepage', { data : x });
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+
+app.get('/my-biking', (req, res) => {
+    res.render('biking');
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`);
+})
+
+
+// catch 404 error
