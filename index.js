b69246fffdaf26f235e1729fee44ea1c102abe90
Skyler Rivera <skylerrivera17@yahoo.com>
2021-01-13 21:07:22 -0500

    updates from 0131, switching to Express and ejs, added local POSTGRES database called nodedb

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const express = require('express');
+const ejs = require('ejs');
+const { Pool, Client } = require('pg')
 
-//create a server object;
+const connectionString = 'postgresql://postgres:postgres@127.0.0.1:5432/nodedb'
+const pool = new Pool({
+  connectionString,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+})
+
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    let x = 15;
+    let firstName = "Skyler";
+    res.render('homepage', { 
+        data : x,
+        name : firstName });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/my-biking', (req, res) => {
+    res.render('my-biking');
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening at http://localhost:3000');
+})
+
+
+
+
+
+
+
+
+
+
+
+/*create a server object;
 http.createServer(function (req, res) { //req = request, res = response
 
     let path = url.parse(req.url).pathname;
@@ -27,4 +71,4 @@ http.createServer(function (req, res) { //req = request, res = response
                 res.write('Route not found');
                 res.end();
         }
-}).listen(8080); //the server object listens on port 8080
+}).listen(8080); */ //the server object listens on port 8080
