0b1037526e382e1546e08b0dae5581efb46e40ae
frick.james <jamesfrick100@yahoo.com>
2021-01-17 14:42:25 -0500

    COM - Completed sqlite example with pug templating.

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Set up the database
+const db = new sqlite3.Database('test.db');
+
 // Routing
 app.get('/', (req, res) => {
     let x = 15;
@@ -24,9 +27,35 @@ app.get('/my-biking', (req, res) => {
     res.render('biking');
 })
 
-app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`);
+app.get('/sqlite-test', (req, res) => {
+    db.serialize(function() {
+        db.all("SELECT id, name, cost FROM product", function(err, rows) {
+            // Get our data
+            var data = {};
+            var i = 0;
+            rows.forEach(function(row) {
+                data[i] = { name: row.name, cost: row.cost };
+                i++;
+            });
+            res.render('data', { data: data });
+        });
+
+    });
 })
 
+app.listen(3000, () => {
+    // create_table();
+    console.log(`Example app listening at http://localhost:3000`);
+})
+
+function create_table() {
+    db.serialize(function() {
+        db.run('CREATE TABLE product (id INT, name TEXT, cost REAL)')
+        let insert = 'INSERT INTO product (id, name, cost) VALUES (?,?,?)';
+        db.run(insert, [1, "Banana", 5.0]);
+        db.run(insert, [2, "Kiwi", 6.5]);
+        db.run(insert, [3, "Apple", 3.0]);
+    })
+}
 
 // catch 404 error
